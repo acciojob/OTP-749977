22deada0dcef5f3a3bc767450fb061d09f8987c0
shivani joshi <161042809+749977@users.noreply.github.com>
2025-03-06 14:22:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-//your JS code here. If required.
+const codes = document.querySelectorAll(".code");
+
+codes.forEach((code, index) => {
+    code.addEventListener("input", (e) => {
+        if (e.target.value.length === 1 && index < codes.length - 1) {
+            codes[index + 1].focus();
+        }
+    });
+
+    code.addEventListener("keydown", (e) => {
+        if (e.key === "Backspace" && index > 0 && e.target.value === "") {
+            codes[index - 1].focus();
+        }
+    });
+});
+
