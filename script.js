4347fe69d9aa13636b2fe260137421aa8d45b42c
shivani joshi <161042809+749977@users.noreply.github.com>
2025-03-07 15:06:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
-const codes = document.querySelectorAll(".code");
+const codes = document.querySelectorAll('.code');
 
 codes.forEach((code, index) => {
-    code.addEventListener("input", (e) => {
-        if (e.target.value.length === 1 && index < codes.length - 1) {
+    code.addEventListener('input', (e) => {
+        if (e.target.value && index < codes.length - 1) {
             codes[index + 1].focus();
         }
     });
 
-    code.addEventListener("keydown", (e) => {
-        if (e.key === "Backspace" && index > 0 && e.target.value === "") {
+    code.addEventListener('keydown', (e) => {
+        if (e.key === 'Backspace' && !e.target.value && index > 0) {
             codes[index - 1].focus();
         }
     });
 });
-
